11192e15c43afdf89f38536089e14e6d5583e5ce
unknown <matthewaomalley@lewisu.edu>
2022-10-18 17:44:24 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Web app with Node.js and express for assignment portfolio
+
+const express = require('express')
+const app = express()
+var url = require('url')
+app.use(express.static(__dirname + '/static'))
+const port = process.env.PORT || 8080;
+
+// listen on the port
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}; ` +
+    `press Ctrl-C to terminate.`))
